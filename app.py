30276b8277982f71d3727a64e7b1c29a1c0712aa
udenis80 <udenis80@mail.ru>
2023-06-01 02:10:17 +0300

    arneus

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route('/single')
+def single():
+    return render_template('single.html')\
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/archive')
+def archive():
+    return render_template('archive.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
