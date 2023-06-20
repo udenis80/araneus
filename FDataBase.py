136a9a1e19b75b71b3ec4979c45076c28d9b1bec
udenis80 <udenis80@mail.ru>
2023-06-21 01:54:37 +0300

    just do

--- FDataBase.py
+++ FDataBase.py
@@ -27,7 +27,7 @@ class FDataBase:
                 print("Статья с таким url уже существует")
                 return False
 
-            base = url_for('static', filename='images_html')
+            base = url_for('static', filename='images')
 
             text = re.sub(r"(?P<tag><img\s+[^>]*src=)(?P<quote>[\"'])(?P<url>.+?)(?P=quote)>",
                           "\\g<tag>" + base + "/\\g<url>>",
